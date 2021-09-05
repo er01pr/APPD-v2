1f645f76c13301d7935451962b93659ac0626cdb
Josh Gripo <joshua.gripo@gmail.com>
2021-09-05 23:06:24 +0800

    Added a database for servers and added it to index.html

--- app.py
+++ app.py
@@ -24,4 +24,14 @@ def home():
     author_list = [author[0] for author in authors]
     print(author_list)
 
-    return render_template("index.html", author_list=author_list)
+    #Read and feth the server in Servers database.
+    serverdb = "SELECT server FROM Servers"
+    servers = cursor.execute(serverdb)
+    servers = servers.fetchall()
+
+    #Convert server tuples to list via list comprehension
+    server_list = [server[0] for server in servers]
+    print(server_list)
+
+
+    return render_template("index.html", author_list=author_list, server_list=server_list)
