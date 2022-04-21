cb99e373f751dd691736659e99127761fc4fb3c1
Josh Gripo <joshua.gripo@gmail.com>
2022-04-21 15:38:16 +0800

    Added libraries in app.py. Added def after_request and Templates Auto Reload

--- app.py
+++ app.py
@@ -2,11 +2,26 @@ from cgitb import html
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 import sqlite3
 import os
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
 
 #Current Directory of the project
 currentdirectory = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
+
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 #Homepage
 @app.route("/")
